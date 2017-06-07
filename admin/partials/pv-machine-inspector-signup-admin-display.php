daa3f1bba778391f140e62310cc8269b76bc14d7
matt murphy <mattyhead@gmail.com>
2017-06-07 10:37:26 -0400

    ...

--- admin/partials/pv-machine-inspector-signup-admin-display.php
+++ admin/partials/pv-machine-inspector-signup-admin-display.php
@@ -35,9 +35,9 @@ do_settings_sections($this->plugin_name);
     <h2><?php echo esc_html(get_admin_page_title()); ?></h2>
 
     <h2 class="nav-tab-wrapper">
-        <a href="#pvmi-list" class="nav-tab <?php echo ($post->action === 'edit') ? '' : 'nav-tab-active'; ?>"><?php _e('List', $this->plugin_name);?></a>
+        <a href="#pvmi-list" class="nav-tab <?php echo ($request->action === 'edit') ? '' : 'nav-tab-active'; ?>"><?php _e('List', $this->plugin_name);?></a>
         <a href="#pvmi-add" class="nav-tab"><?php _e('Add', $this->plugin_name);?></a>
-        <?php if ($post->action === 'edit') : ?>
+        <?php if ($request->action === 'edit') : ?>
         <a href="#pvmi-edit" class="nav-tab nav-tab-active"><?php _e('Edit', $this->plugin_name);?></a>
         <?php endif; ?>
         <a href="#pvmi-config" class="nav-tab"><?php _e('Config', $this->plugin_name);?></a>
@@ -47,7 +47,7 @@ do_settings_sections($this->plugin_name);
     // Include tabs partials
     require_once('pv-machine-inspector-signup-admin-list.php');
     require_once('pv-machine-inspector-signup-admin-add.php');
-    if ($post->action === 'edit') {
+    if ($request->action === 'edit') {
         require_once('pv-machine-inspector-signup-admin-edit.php');
     }
     require_once('pv-machine-inspector-signup-admin-config.php');
