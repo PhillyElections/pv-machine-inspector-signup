4f930d530b55afbb3e8e341b9bcf7d82c81cae74
matt murphy <mattyhead@gmail.com>
2017-06-07 15:45:58 -0400

    ...

--- admin/class-pv-machine-inspector-signup-admin.php
+++ admin/class-pv-machine-inspector-signup-admin.php
@@ -144,7 +144,7 @@ class Pv_Machine_Inspector_Signup_Admin {
 
 	static public function add( ) {
 		//$this->get_models( 'pv_mi_signups' );
-		ddd('ADD', $this, dirname( dirname( __FILE__ ) ) . '/db/pv-model-factory.php');
+		ddd('ADD', $this, , $this->get_models( 'pv_mi_signups' ), dirname( dirname( __FILE__ ) ) . '/db/pv-model-factory.php');
 	}
 
 	public function config( ) {
@@ -156,7 +156,8 @@ class Pv_Machine_Inspector_Signup_Admin {
 	}
 
 	static public function get_models( $tablename ) {
-		d('get_models', $tablename, dirname( dirname( __FILE__ ) ) . '/db/pv-model-factory.php');
+		return $tablename;
+		//d('get_models', $tablename, dirname( dirname( __FILE__ ) ) . '/db/pv-model-factory.php');
 //		require_once dirname( dirname( __FILE__ ) ) . '/db/pv-model-factory.php';
 
 	}
