e8d29be1f7c5840127bb674d98107686f2ebde8b
matt murphy <mattyhead@gmail.com>
2017-06-12 15:10:40 -0400

    ...

--- admin/js/pv-machine-inspector-signup-admin.js
+++ admin/js/pv-machine-inspector-signup-admin.js
@@ -5,28 +5,31 @@
 
         // tabs
         var $tabBoxes = $('.pvmi-metaboxes'),
-               $tabLinkActive,
-               $currentTab,
-               $currentTabLink,
-               $tabContent,
-               $hash;
+            $navTabWrapper = $('.nav-tab-wrapper'),
+            $navTab = $('.nav-tab'),
+            $tabLinkActive,
+            $currentTab,
+            $currentTabLink,
+            $tabContent,
+            $hash;
 
         // Tabs on load
         if((w.location.hash).length){
             $hash = w.location.hash;
             $tabBoxes.addClass('hidden');
             $currentTab = $($hash).toggleClass('hidden');
-            $('.nav-tab').removeClass('nav-tab-active');
+            $navTab.removeClass('nav-tab-active');
             $('.nav-tab[href='+$hash+']').addClass('nav-tab-active');
         }
         //Tabs on click
-        $('.nav-tab-wrapper').on('click', 'a', function(e){
+        $naTabWrapper.on('click', 'a', function(e){
+            var $this = $(this);
             e.preventDefault();
-            $tabContent = $(this).attr('href');
-            $('.nav-tab').removeClass('nav-tab-active');
+            $tabContent = $this.attr('href');
+            $navTab.removeClass('nav-tab-active');
             $tabBoxes.addClass('hidden');
             $currentTab = $($tabContent).toggleClass('hidden');
-            $(this).addClass('nav-tab-active');
+            $this.addClass('nav-tab-active');
              if(history.pushState) {
                 history.pushState(null, null, $tabContent);
             }
