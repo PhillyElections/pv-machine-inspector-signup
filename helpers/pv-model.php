ac328de01eb657f731f416196de108cb82d90987
matt murphy <mattyhead@gmail.com>
2017-06-16 15:13:46 -0400

    ...

--- helpers/pv-model.php
+++ helpers/pv-model.php
@@ -73,8 +73,8 @@ class Pv_Model {
 
     private function _fetch_paged_sql( ) {
 
-        $sql = sprintf( 'SELECT * FROM %s LIMIT %%s, %%s', $this->_table());
-        return $this->db->prepare( $sql, $this->pagination->start, $this->pagination->end );
+        $sql = sprintf( 'SELECT * FROM %s LIMIT %s, %s ', $this->_table() $this->pagination->start, $this->pagination->end );
+        return $sql;
     }
 
     public function get_row( $value ) {
