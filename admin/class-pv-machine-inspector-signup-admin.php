d38f3844cdc844263c34c7dbaa269bc8d986f63d
matt murphy <mattyhead@gmail.com>
2017-06-16 14:02:51 -0400

    adding model assignment by ref

--- admin/class-pv-machine-inspector-signup-admin.php
+++ admin/class-pv-machine-inspector-signup-admin.php
@@ -56,8 +56,9 @@ class Pv_Machine_Inspector_Signup_Admin {
 	 * @param      string    $plugin_name       The name of this plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
-	public function __construct( $plugin_name, $version ) {
+	public function __construct( $plugin_name, $version, &$models ) {
 
+		$this->models =& $models;
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
 
