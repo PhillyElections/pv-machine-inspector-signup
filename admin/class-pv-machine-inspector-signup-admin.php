6f1e52d963312a5cf13cf9983e30aaf94ea07c55
matt murphy <mattyhead@gmail.com>
2017-06-16 11:53:09 -0400

    ...

--- admin/class-pv-machine-inspector-signup-admin.php
+++ admin/class-pv-machine-inspector-signup-admin.php
@@ -129,7 +129,7 @@ class Pv_Machine_Inspector_Signup_Admin {
 		if ($GLOBALS['admin_page_hooks']['phillyvotes']) {
 
 		} else {
-			add_menu_page(__('Phillyvotes', $this->plugin_name),'Phillyvotes','manage_options','phillyvotes', array($this, 'display_plugin_manage_display_page' );
+			add_menu_page(__('Phillyvotes', $this->plugin_name),'Phillyvotes','manage_options','phillyvotes', array($this, 'display_plugin_manage_display_page') );
 		}
 		add_submenu_page('phillyvotes',__('Machine Inspector Signup', $this->plugin_name), 'Machine Inspectors', 'manage_options', $this->plugin_name, array($this, 'display_plugin_manage_display_page')
 		);
