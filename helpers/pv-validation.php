be3b16f68fc8619700dc1b5ccdffadd4cc1809be
matt murphy <mattyhead@gmail.com>
2017-06-21 08:07:40 -0400

    validation catchup

--- helpers/pv-validation.php
+++ helpers/pv-validation.php
@@ -9,20 +9,27 @@
  * @subpackage Pv_Elections_Core/db
  * @author     matthew murphy <matthew.e.murphy@phila.gov>
  */
-if ( ! class_exists('Pv_Validation') ) {
+if ( ! class_exists( 'Pv_Validation' ) ) {
 	class Pv_Validation {
 
 		private $data;
 		private $messages;
-		private $rules;
-		private $scrubable;
+		private $processing;
+		private $scrubbable;
+		private $plugin;
+		private $version;
+
+		public function __construct( $plugin, $version ) {
+			$this->plugin = $plugin;
+			$this->version = $version;
+		}
 
 		/**
 		 * Gets the value of messages.
 		 *
 		 * @return mixed
 		 */
-		public function getMessages() {
+		public function getMessages( ) {
 			return $this->messages;
 		}
 
@@ -31,7 +38,7 @@ if ( ! class_exists('Pv_Validation') ) {
 		 *
 		 * @return mixed
 		 */
-		public function getData() {
+		public function getData( ) {
 			return $this->data;
 		}
 
@@ -40,7 +47,7 @@ if ( ! class_exists('Pv_Validation') ) {
 		 *
 		 * @return mixed
 		 */
-		public function getRules() {
+		public function getRules( ) {
 			return $this->rules;
 		}
 
@@ -49,9 +56,9 @@ if ( ! class_exists('Pv_Validation') ) {
 		 *
 		 * @return mixed
 		 */
-		public function setMessage($value) {
+		public function setMessage( $value ) {
 			if ( ! count( $this->messages ) ) {
-				$this->messages = array($value);
+				$this->messages = array( $value );
 				return;
 			}
 
@@ -63,16 +70,18 @@ if ( ! class_exists('Pv_Validation') ) {
 		 *
 		 * @return mixed
 		 */
-		public function setData($data) {
-			$this->data = $data;
-		}
+		public function setData( ) {
+			$data = $this->filter( $_REQUEST );
+			$this->data = $this->scrubbable ? $this->scrub( $data ) : $data;
+
+			unset( $data );
 
 		/**
 		 * Set the rules.
 		 *
 		 * @return mixed
 		 */
-		public function setRules($rules) {
+		public function setRules( $rules ) {
 			$this->rules = $rules;
 		}
 
@@ -81,36 +90,56 @@ if ( ! class_exists('Pv_Validation') ) {
 		 *
 		 * @return     bool  success of the validation operation overall
 		 */
-		public function run() {
+		public function run( ) {
 			$valid = true;
-			$data = $this->scrubable ? $this->scrub( $this->data ) : &$this->data;
-			$rules = &$this->rules;
+			$invaliated = array();
 
-			// whitespace scrub for all
-			$data = $this->scrub( $data );
+			$this->setData( );
 
-			// loop through fields
-			foreach ( $data as $field=>&$value ) {
-				// is this required? 
+			foreach ( $this->processing as $field=>$process ) {
+				// start with requirement and existence
+				if ( $process['required'] ) {
+					// element not set at all or is falsy
+					if ( !isset($this->data[$field]) || !$this->data['field']) {
+						$valid = false;
+						$this->addMessage( $process['label'] . __( ' is required.'), $this->plugin );
+						// go on to next field
+						continue;
+					}
+
+				}
+
+				// initialize $function
+				$function = '';
+
+				// leep through assigned sanitize functions
+				foreach ( $process[$field]['sanitize'] as $function ) {
+					if ( method_exists( $this, $function ) ) {
+						// let's run our extant method, $function
+						$this->$function( $this->data[$field] );
+					}
+				}
+
+				// re-initialize $function
+				$function = '';
 				
-				// apply rules to each
+				// loop through assigned validation functions
+				foreach ( $process[$field]['validate'] as $function ) {
+					if ( method_exists( $this, $function ) ) {
+						if ( !$this->$function($this->data[$field]) ) {
+							$this->addMessage( $process[$field]['label'] . __( ' failed validation: ' ) . $function );
+							$valid=false;
+							array_push($invalidated, array($field,$function));
+						}
+					}
+				}
 
 			}
-			 
 			return $valid;
 		}
 
 		/**
-		 * Clean properties
-		 */
-		public function reset() {
-			$this->messages = null;
-			$this->data = null;
-			$this->rules = rules;
-		}
-
-		/**
-		 * shim for PHP ctype_alpha()
+		 * shim for PHP ctype_alpha( )
 		 *
 		 * @param      string   $value  a possible alphabetic value
 		 *
@@ -121,7 +150,7 @@ if ( ! class_exists('Pv_Validation') ) {
 		}
 
 		/**
-		 * negating shim for PHP empty()
+		 * negating shim for PHP empty( )
 		 *
 		 * @param      string   $value  a possible non-empty value
 		 *
@@ -132,7 +161,7 @@ if ( ! class_exists('Pv_Validation') ) {
 		}
 
 		/**
-		 * shim for PHP is_numeric().
+		 * shim for PHP is_numeric( ).
 		 *
 		 * @param      string   $value  a possible numeric value
 		 *
@@ -161,82 +190,129 @@ if ( ! class_exists('Pv_Validation') ) {
 		 *
 		 * @return     boolean  True if us state, False otherwise.
 		 */
-		public function is_us_state($value) {
-'Alabama',
-'Alaska',
-'American Samoa',
-'Arizona',
-'Arkansas',
-'California',
-'Colorado',
-'Connecticut',
-'Delaware',
-'District of Columbia',
-'Federated States of Micronesia',
-'Florida',
-'Georgia',
-'Guam',
-'Hawaii',
-'Idaho',
-'Illinois',
-'Indiana',
-'Iowa',
-'Kansas',
-'Kentucky',
-'Louisiana',
-'Maine',
-'Marshall Islands',
-'Maryland',
-'Massachusetts',
-'Michigan',
-'Minnesota',
-'Mississippi',
-'Missouri',
-'Montana',
-'Nebraska',
-'Nevada',
-'New Hampshire',
-'New Jersey',
-'New Mexico',
-'New York',
-'North Carolina',
-'North Dakota',
-'Northern Mariana Islands',
-'Ohio',
-'Oklahoma',
-'Oregon',
-'Palau',
-'Pennsylvania',
-'Puerto Rico',
-'Rhode Island',
-'South Carolina',
-'South Dakota',
-'Tennessee',
-'Texas',
-'Utah',
-'Vermont',
-'Virgin Islands',
-'Virginia',
-'Washington',
-'West Virginia',
-'Wisconsin',
-'Wyoming',
+		public function is_us_state( $value ) {
+			$states = array( 
+				'Alabama',
+				'Alaska',
+				'American Samoa',
+				'Arizona',
+				'Arkansas',
+				'California',
+				'Colorado',
+				'Connecticut',
+				'Delaware',
+				'District of Columbia',
+				'Federated States of Micronesia',
+				'Florida',
+				'Georgia',
+				'Guam',
+				'Hawaii',
+				'Idaho',
+				'Illinois',
+				'Indiana',
+				'Iowa',
+				'Kansas',
+				'Kentucky',
+				'Louisiana',
+				'Maine',
+				'Marshall Islands',
+				'Maryland',
+				'Massachusetts',
+				'Michigan',
+				'Minnesota',
+				'Mississippi',
+				'Missouri',
+				'Montana',
+				'Nebraska',
+				'Nevada',
+				'New Hampshire',
+				'New Jersey',
+				'New Mexico',
+				'New York',
+				'North Carolina',
+				'North Dakota',
+				'Northern Mariana Islands',
+				'Ohio',
+				'Oklahoma',
+				'Oregon',
+				'Palau',
+				'Pennsylvania',
+				'Puerto Rico',
+				'Rhode Island',
+				'South Carolina',
+				'South Dakota',
+				'Tennessee',
+				'Texas',
+				'Utah',
+				'Vermont',
+				'Virgin Islands',
+				'Virginia',
+				'Washington',
+				'West Virginia',
+				'Wisconsin',
+				'Wyoming',
+				'AL',
+				'AK',
+				'AS',
+				'AZ',
+				'AR',
+				'CA',
+				'CO',
+				'CT',
+				'DE',
+				'DC',
+				'FM',
+				'FL',
+				'GA',
+				'GU',
+				'HI',
+				'ID',
+				'IL',
+				'IN',
+				'IA',
+				'KS',
+				'KY',
+				'LA',
+				'ME',
+				'MH',
+				'MD',
+				'MA',
+				'MI',
+				'MN',
+				'MS',
+				'MO',
+				'MT',
+				'NE',
+				'NV',
+				'NH',
+				'NJ',
+				'NM',
+				'NY',
+				'NC',
+				'ND',
+				'MP',
+				'OH',
+				'OK',
+				'OR',
+				'PW',
+				'PA',
+				'PR',
+				'RI',
+				'SC',
+				'SD',
+				'TN',
+				'TX',
+				'UT',
+				'VT',
+				'VI',
+				'VA',
+				'WA',
+				'WV',
+				'WI',
+				'WY',
+			 );
 
-
-
-
-
-
-
-
-
-
-			$states = array(
-
-
-			);
-
-			return in_array($value, $states);
+			return in_array( $value, $states );
 		}
 
 		/**
@@ -246,12 +322,12 @@ if ( ! class_exists('Pv_Validation') ) {
 		 *
 		 * @return     boolean  True if us zip code, False otherwise.
 		 */
-		public function is_us_zip_code($value) {
-			if (strlen(trim($value)) > 10) {
+		public function is_us_zip_code( $value ) {
+			if ( strlen( trim( $value ) ) > 10 ) {
 				return false;
 			}
 		 
-			if (!preg_match('/^\d{5}(\-?\d{4})?$/', $value)) {
+			if ( !preg_match( '/^\d{5}( \-?\d{4} )?$/', $value ) ) {
 				return false;
 			}
 		 
@@ -259,25 +335,25 @@ if ( ! class_exists('Pv_Validation') ) {
 		}
 
 		/**
-		 * Scrub whitespace (trim() plus no multiple \t|\n|\s)
+		 * Scrub whitespace ( trim( ) plus no multiple \t|\n|\s )
 		 *
 		 * @param      mixed  $data   all the form data
 		 *
 		 * @return     mixed  a less whitespacey $data array
 		 */
-		public function scrub( &$data ) {
+		public function scrub( $data ) {
 			array_walk( $data, 
 				function ( &$value ) {
 					$value = trim( $value );
 					$value = preg_replace( '!\s+!', ' ', $value );
 				}
-			);
+			 );
 
 			return $data;
 		}
 
 		/**
-		 * shim for WP sanitize_email()
+		 * shim for WP sanitize_email( )
 		 *
 		 * @param      string  $value  a possible email
 		 *
@@ -300,7 +376,7 @@ if ( ! class_exists('Pv_Validation') ) {
 		}
 
 		/**
-		 * shim for WP sanitize_text_field()
+		 * shim for WP sanitize_text_field( )
 		 *
 		 * @param      string  $value  text input value
 		 *
@@ -312,4 +388,13 @@ if ( ! class_exists('Pv_Validation') ) {
 
 	}
 
+    public function filter ( $data ) {
+
+        foreach ( $data as $key->$value ) {
+            if ( array_key_exists( $key, $this->processing ) ) {
+                $filtered[$key] = $value;
+            }
+        }
+        return $filtered;
+    }
 }
