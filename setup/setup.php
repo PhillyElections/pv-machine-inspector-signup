9ad68a00d8d916b6b467e8569ee42b3c227d59e5
matt murphy <mattyhead@gmail.com>
2017-07-11 08:19:07 -0400

    dropping statics

--- setup/setup.php
+++ setup/setup.php
@@ -28,7 +28,7 @@ class Pv_Machine_Inspector_Signup_Db {
 	 * @access   private
 	 * @var      string    $plugin_name    The ID of this plugin.
 	 */
-	private $plugin_name;
+	public $plugin_name;
 
 	/**
 	 * The version of this plugin.
@@ -37,7 +37,7 @@ class Pv_Machine_Inspector_Signup_Db {
 	 * @access   private
 	 * @var      string    $version    The current version of this plugin.
 	 */
-	private $version;
+	public $version;
 
 	/**
 	 * Initialize the class and set its properties.
