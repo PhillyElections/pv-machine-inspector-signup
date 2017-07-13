feb285b0cfbbe450fb45cebeb08664256ba975f4
matt murphy <mattyhead@gmail.com>
2017-07-13 08:46:23 -0400

    Let's not even bother doing configuration stuff admin-side

---
+++ includes/class-pv-machine-inspector-signup-configurator.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Fired during plugin activation
+ *
+ * @link       philadelphiavotes.com
+ * @since      1.0.0
+ *
+ * @package    Pv_Machine_Inspector_Signup
+ * @subpackage Pv_Machine_Inspector_Signup/includes
+ */
+
+if ( ! class_exists( 'Pv_Machine_Inspector_Signup_Configurator' ) ) {
+	/**
+	 * Fired during plugin activation.
+	 *
+	 * This class defines all code necessary to run during the plugin's activation.
+	 *
+	 * @since      1.0.0
+	 * @package    Pv_Machine_Inspector_Signup
+	 * @subpackage Pv_Machine_Inspector_Signup/includes
+	 * @author     matthew murphy <matthew.e.murphy@phila.gov>
+	 */
+	class Pv_Machine_Inspector_Signup_Configurator {
+
+	}	
+}
