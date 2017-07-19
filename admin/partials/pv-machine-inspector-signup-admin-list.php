71267171625e35e69bdcd3b369297a317680a275
matt murphy <mattyhead@gmail.com>
2017-07-19 15:55:13 -0400

    ...

--- admin/partials/pv-machine-inspector-signup-admin-list.php
+++ admin/partials/pv-machine-inspector-signup-admin-list.php
@@ -57,11 +57,11 @@ $paginator->setup( $this->plugin_name, $this->models->signups->get_pagination() 
 
 		foreach ( $rows as $row ) :
 			$i++;
-			$delete_link    = admin_url( 'admin-post.php?action=pvmi_admin_delete&item=' . $row->id );
-			$edit_link      = admin_url( 'admin.php?page=' . $this->plugin_name . '&action=edit&item=' . $row->id );
-			$base_post = admin_url( 'admin_post.php' );
-			$fullname  = $row->first_name . ' ' . ( $row->middle_name ? $row->middle_name . ' ' : '' ) . $row->last_name;
-			$matches   = '';
+			$delete_link = admin_url( 'admin-post.php?action=pvmi_admin_delete&item=' . $row->id );
+			$edit_link   = admin_url( 'admin.php?page=' . $this->plugin_name . '&action=edit&item=' . $row->id );
+			$base_post   = admin_url( 'admin_post.php' );
+			$fullname    = $row->first_name . ' ' . ( $row->middle_name ? $row->middle_name . ' ' : '' ) . $row->last_name;
+			$matches     = '';
 			preg_match( '/^(\d{3})(\d{3})(\d{4})$/', $row->phone, $matches );
 		?>
 			<tr>
@@ -105,7 +105,7 @@ $paginator->setup( $this->plugin_name, $this->models->signups->get_pagination() 
 				<td colspan="9">
 					<?php echo esc_html( $paginator->get_list_footer() ); ?>
 					<span class="row-actions visible">
-						<span><a href="<?php echo esc_attr( admin_url( 'admin.php?page=' . $this->plugin_name . '&action=export') ); ?>" >export all</a></span>
+						<span><a href="<?php echo esc_attr( admin_url( 'admin.php?page=' . $this->plugin_name . '&action=export' ) ); ?>" >export all</a></span>
 						<span>|</span>
 						<span class="trash"><a href="<?php echo esc_attr( admin_url( 'admin-post.php?action=pvmi_admin_delete_all' ) ); ?>" >delete all</a></span>
 					</span>
