287ade17af09f32436118803813fd4bf7ce5a84d
matt murphy <mattyhead@gmail.com>
2017-07-24 14:27:48 -0400

    ...

--- admin/partials/pv-machine-inspector-signup-admin-list.php
+++ admin/partials/pv-machine-inspector-signup-admin-list.php
@@ -105,7 +105,7 @@ $paginator->setup( $this->plugin_name, $this->models->signups->get_pagination() 
 				<td colspan="9">
 					<?php echo esc_html( $paginator->get_list_footer() ); ?>
 					<span class="row-actions visible">
-						<span><a target="_blank" href="<?php echo WP_PLUGIN_URL . $this->plugin_name . '/export.php'; ?>" >export all</a></span>
+						<span><a target="_blank" href="<?php echo WP_PLUGIN_URL . '/' . $this->plugin_name . '/export.php'; ?>" >export all</a></span>
 						<span>|</span>
 						<span class="trash"><a href="<?php echo esc_attr( admin_url( 'admin-post.php?action=pvmi_admin_delete_all' ) ); ?>" >delete all</a></span>
 					</span>
