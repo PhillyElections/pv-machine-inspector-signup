822cc0695fc32840e4874dbb4d5fe5fb7c142790
matt murphy <mattyhead@gmail.com>
2017-07-24 13:44:38 -0400

    ...

--- admin/partials/pv-machine-inspector-signup-admin-export.php
+++ admin/partials/pv-machine-inspector-signup-admin-export.php
@@ -11,26 +11,10 @@
  * @subpackage Pv_Machine_Inspector_Signup/admin/partials
  */
 
-if ( ! headers_sent() ) {
-	foreach ( headers_list() as $header ) {
-		header_remove( $header );
-	}
-}
-
 $export_filename = date( 'Y-m-d' ) . '_appliants_export.csv';
 
-header( 'Content-type: application/csv' );
-header( 'Content-Disposition: attachment; filename="' . $csv_filename . '"' );
-header( 'Pragma: public' );
-header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' ); // Date in the past.
-header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
-header( 'Cache-Control', 'no-store, no-cache, must-revalidate' ); // HTTP/1.1.
-header( 'Cache-Control: pre-check=0, post-check=0, max-age=0' ); // HTTP/1.1.
-header( 'Pragma: no-cache' );
-header( 'Expires: 0' );
-header( 'Content-Transfer-Encoding: none' );
-header( 'Content-Type: application/csv' ); // joomla will overwrite this...
-header( 'Content-Disposition: attachment; filename="' . $export_filename . '"' );
+header( 'Content-Type:application/csv' ); // joomla will overwrite this...
+header( 'Content-Disposition:attachment;filename=' . $export_filename );
 
 $output = fopen( 'php://output', 'w' );
 
