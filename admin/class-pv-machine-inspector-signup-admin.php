a5a5f0fac36db85c2d97582732e39b6483816baa
matt murphy <mattyhead@gmail.com>
2017-07-25 15:23:34 -0400

    ...

--- admin/class-pv-machine-inspector-signup-admin.php
+++ admin/class-pv-machine-inspector-signup-admin.php
@@ -290,7 +290,7 @@ class Pv_Machine_Inspector_Signup_Admin {
 
 				$validator = &$this->validator['signups'];
 
-				dd( $validator->run() /*, $validator*/ , $validator->get_messages()[0] /*, $validator->get_data()*/ );
+				dd($validator->run(), $validator, $validator->get_messages()[0], $validator->get_data());
 
 				if ( ! $this->models->signups->update( $data, array( 'id' => $item ) ) ) {
 					$status = 'error';
